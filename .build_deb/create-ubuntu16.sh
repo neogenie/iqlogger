c1e442b94b24d569a4dd8b36521820a34c8fdf11
Neo <neogenie@yandex.ru>
2020-01-28 15:01:07 +0300

    Add Travis CI

--- .build_deb/create-ubuntu16.sh
+++ .build_deb/create-ubuntu16.sh
@@ -2,10 +2,10 @@
 
 VERSION=`cat version`
 
-rm .build_deb/ubuntu-16/var/log/iqlogger/.gitkeep
-rm .build_deb/ubuntu-16/var/lib/iqlogger/.gitkeep
-rm .build_deb/ubuntu-16/usr/local/bin/.gitkeep
-cd .build_deb/ubuntu-16
+rm .build_deb/ubuntu16/var/log/iqlogger/.gitkeep
+rm .build_deb/ubuntu16/var/lib/iqlogger/.gitkeep
+rm .build_deb/ubuntu16/usr/local/bin/.gitkeep
+cd .build_deb/ubuntu16
 fpm -s dir -t deb -a amd64 -n iqlogger -v ${VERSION}-xenial -m neogenie@yandex.ru \
     -d libtbb2 -d zlib1g \
     --pre-install ../pre-install.sh \
--- .build_deb/create-ubuntu18.sh
+++ .build_deb/create-ubuntu18.sh
@@ -2,10 +2,10 @@
 
 VERSION=`cat version`
 
-rm .build_deb/ubuntu-18/var/log/iqlogger/.gitkeep
-rm .build_deb/ubuntu-18/var/lib/iqlogger/.gitkeep
-rm .build_deb/ubuntu-18/usr/local/bin/.gitkeep
-cd .build_deb/ubuntu-18
+rm .build_deb/ubuntu18/var/log/iqlogger/.gitkeep
+rm .build_deb/ubuntu18/var/lib/iqlogger/.gitkeep
+rm .build_deb/ubuntu18/usr/local/bin/.gitkeep
+cd .build_deb/ubuntu18
 fpm -s dir -t deb -a amd64 -n iqlogger -v ${VERSION}-bionic -m neogenie@yandex.ru \
     -d libtbb2 -d zlib1g \
     --pre-install ../pre-install.sh \
